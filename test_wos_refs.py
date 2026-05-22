41e3df2079813e767dcf1a466cd5ffdc5d954214
Benjamin Pillot <benjaminpillot@riseup.net>
2026-05-22 20:51:44 +0200

    Remove useless python files from tracking

--- test_wos_refs.py
+++
@@ -1,9 +0,0 @@
-from pysyrev.bibdata import BibDataset, WosDataset
-from pysyrev.core.config import Config
-
-config = Config.load("pysyrev/config_examples/config_abm.yaml")
-# wos_bib = WosDataset.from_config(config.bib.wos)
-bib = BibDataset.from_config(config.bib)
-# bib.clean_and_drop().resolve_references()
-
-# bib.to_csv("/home/benjaminpillot/Documents/PRO/ABM_LITERATURE_REVIEW/PYSYREV/wos_oa.csv")
